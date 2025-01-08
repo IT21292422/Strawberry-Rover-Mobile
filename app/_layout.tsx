f622089f4fc1d24d089fde47bbe9b6873b7bb3a1
IT21292422 <it21292422@my.sliit.lk>
2025-01-08 23:32:22 +0530

    feat: added tanstack and rover testing screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,16 @@
 import { Slot } from "expo-router";
 import "../global.css";
 import { PaperProvider } from "react-native-paper";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function RootLayout() {
+  const queryClient = new QueryClient();
+
   return (
-    <PaperProvider>
-      <Slot />
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <Slot />
+      </PaperProvider>
+    </QueryClientProvider>
   );
 }
